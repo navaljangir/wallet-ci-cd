1dda49868626ff97fc64e56f85d22c554d0ad586
Naval Kishor <jangirnaval392003@gmail.com>
2025-01-13 11:58:48 +0530

    updated AddMoney

--- apps/user-app/components/AddMoneyCard.tsx
+++ apps/user-app/components/AddMoneyCard.tsx
@@ -1,11 +1,13 @@
 "use client"
 import { Button } from "@repo/ui/button";
 import { Card } from "@repo/ui/card";
-import { Center } from "@repo/ui/center";
 import { Select } from "@repo/ui/select";
 import { useState } from "react";
 import { TextInput } from "@repo/ui/textinput";
 import { createOnRampTransaction } from "../app/lib/actions/createOnRamptxn";
+
+
+
 
 const SUPPORTED_BANKS = [{
     name: "HDFC Bank",
